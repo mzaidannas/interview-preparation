03ded372adfa3bafee073248e7102b873c8f2d1e
Zaid Annas <m.zaid.annas@gmail.com>
2022-08-13 15:40:56 +0500

    Added minimum absolute sum solution

---
+++ abs_sum.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+# Function to calculate minimum
+# possible sum of all (arr[i] - b + i)
+def MinSum(arr, range)
+  # Modify the array
+  range.times do |i|
+    arr[i] -= (i + 1)
+  end
+
+  # sort the array
+  arr.sort
+
+  # calculate median
+  b = arr[range / 2]
+
+  # Stores the required answer
+  ans = 0
+  range.times do |i|
+    # Update answer
+    ans += (arr[i] - b).abs
+  end
+
+  # Return the answer
+  ans
+end
+
+# Driver code
+arr = [1, 5, 2, -2]
+range = arr.length
+puts(MinSum(arr, range))
