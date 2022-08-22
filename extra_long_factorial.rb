9f70ded7f0d109fb728eed6ccbd4bf6625e57ec2
Zaid Annas <m.zaid.annas@gmail.com>
2022-08-22 21:15:31 +0500

    Added extra long factorial

---
+++ extra_long_factorial.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# @param n {Integer} Number to find factorial of
+# @return {Integer} Factorial of n
+def extra_long_factorial(n, previous = {})
+  return n if n <= 1
+
+  ans = (previous[n - 1] || extra_long_factorial(n - 1, previous)) * n
+  previous[n] = ans
+  ans
+end
+
+puts extra_long_factorial(88) # => 185482642257398439114796845645546284380220968949399346684421580986889562184028199319100141244804501828416633516851200000000000000000000
