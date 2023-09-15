c06f6f9ce87eed1c4506b7a5c79d5453621b18ff
Zaid Annas <m.zaid.annas@gmail.com>
2023-09-15 13:19:31 +0500

    Added kth highest element and prefix sum

---
+++ kth_highest_element_in_bst.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+
+# Find Kth laegest element in binary search tree
+
+Node = Struct.new :info, :left, :right, :level do
+  def to_s
+    info
+  end
+end
+
+class BinarySearchTree
+  attr_accessor :root
+
+  def initialize
+    self.root = nil
+  end
+
+  def create(val)
+    if root.nil?
+      self.root = Node.new val
+    else
+      current = root
+
+      loop do
+        if val < current.info
+          if current.left
+            current = current.left
+          else
+            current.left = Node.new val
+            break
+          end
+        elsif val > current.info
+          if current.right
+            current = current.right
+          else
+            current.right = Node.new val
+            break
+          end
+        else
+          break
+        end
+      end
+    end
+  end
+end
+
+def kth_highest(root, k)
+  return -1 if root.nil?
+
+  while k > 0
+    start = root
+    end_ = root.right
+    unless end_.nil?
+      k -= 1
+      root = root.left
+      next
+    end
+
+    while end_.left.present?
+      end_ = end_.left
+    end
+
+  end_.left = start
+  root = root.right
+  root.right = nil
+  end
+
+  start
+end
+
+tree = BinarySearchTree.new
+t = gets.chomp.to_i
+
+arr = gets.chomp.split.map(&:to_i)
+
+(0..t - 1).each do |i|
+  tree.create(arr[i])
+end
+
+puts kth_highest(tree.root, 3)
---
+++ prefix_sum.rb
@@ -0,0 +1,28 @@
+n = 5
+queries = [
+  1,
+  2,
+  100,
+  2,
+  5,
+  100,
+  3,
+  4,
+  100
+]
+
+# Write your code here
+ans = Array.new(n, 0)
+queries.each do |i|
+  ans[i[0] - 1] += i[2]
+  if i[1] < ans.length
+    ans[i[1]] -= i[2]
+  end
+end
+s = 0
+max = 0
+ans.each do |ele|
+  s += ele
+  max = [max, s].max
+end
+puts max
